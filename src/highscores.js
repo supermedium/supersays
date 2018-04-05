669f6f5af5bbee077fcc586cea5afb04e1ee1c07
Diego F. Goberna <diego@feiss.be>
2018-04-05 12:53:39 +0200

    retrieving highscores from firebase

--- src/highscores.js
+++ src/highscores.js
@@ -1,13 +1,25 @@
 AFRAME.registerComponent('highscores', {
   schema: {
-    spacing: {default: 1.0},
-    scorePad: {default: 6}
+    spacing: {default: 1.0}
   },
+
   init: function(){
+    this.scores = null;
+    this.db = firebase.database().ref('scores').orderByValue().limitToFirst(10);
+    this.db.on('value', this.updateScore.bind(this), this.updateError.bind(this));
   },
-  pad: function(n, pad){
-    n = n + '';
-    return n.length >= pad ? n : new Array(pad - n.length + 1).join('0') + n;
+  updateScore: function(data){
+    var data = data.val()
+    this.scores = Object.values(data);
+    this.scores.sort(function(a,b){ return a['score'] < b['score'] ? 1 : a['score'] > b['score'] ? -1 : 0})
+    console.log(this.scores)
+    this.update();
+  }, 
+  updateError: function(err){
+    console.log(err);
+  },
+  saveScore: function(name, score){
+    this.db.push( {name: name.substr(0, 3).toUpperCase(), score: parseInt(score) });
   },
   update: function(oldData){
     var rows = this.el.children;
@@ -15,11 +27,13 @@ AFRAME.registerComponent('highscores', {
     for (var i = 0; i < rows.length; i++) {
       var player = rows[i].querySelector('.player');
       var score = rows[i].querySelector('.score');
-      player.setAttribute('text', {value: 'noname', color: color.getHex()});
-      score.setAttribute('text', {value: this.pad(0, this.data.scorePad), color: color.getHex()});
+      var data = this.scores ? this.scores[i] : null;
+      player.setAttribute('text', {value: data ? data.name : 'noname', color: color.getHex()});
+      score.setAttribute('text', {value: data ? data.score : 0, color: color.getHex()});
       player.setAttribute('position', {y: - i * this.data.spacing});
       score.setAttribute('position', {y: - i * this.data.spacing});
       color.offsetHSL(0.08, 0, 0);
     }
   }
 });
+
