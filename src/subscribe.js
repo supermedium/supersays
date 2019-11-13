8a095fb4c97ca49c44ce9fd7c500625ddf8eb306
Kevin Ngo <me@ngokevin.com>
2019-11-13 15:37:26 -0800

    source

--- src/subscribe.js
+++ src/subscribe.js
@@ -44,7 +44,8 @@ function initSubscribeForm () {
 
     xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     xhr.send(JSON.stringify({
-      email: document.querySelector('[name="email"]').value
+      email: document.querySelector('[name="email"]').value,
+      source: 'supersays'
     }));
 
     return false;
